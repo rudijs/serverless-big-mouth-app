367e7ff8388d34328d193f4899625ab52ba9e2ee
Rudi <rudi.starcevic@gmail.com>
2019-06-09 12:23:07 +0800

    Add aws_iam auth to get-restaurants api

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ Demo serverless framework app
 
 ## Install
 
+- `npm i -g serverless`
 - `git clone git@github.com:rudijs/serverless-big-mouth-app.git`
 - `cd serverless-big-mouth-app`
+- `npm install`
 
 ## Deploy
 
@@ -14,6 +16,10 @@ Create a completely new serverless app:
 - `sls deploy`
 - `node scripts/seed-restaurants.js`
 - Update the `restaurants_api` in serverless.yaml
+- `sls deploy`
+- Update the `restaurants_api` in serverless.yml for the `get-index` function
+- `sls info`
+- After update deploy again
 - `sls deploy`
 
 Update a particular function:
--- functions/get-index.js
+++ functions/get-index.js
@@ -6,6 +6,8 @@ const Mustache = require("mustache");
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const http = require("superagent-promise")(require("superagent"), Promise);
 const restaurantsApiRoot = process.env.restaurants_api;
+const aws4 = require("aws4");
+const URL = require("url");
 
 var html;
 
@@ -16,11 +18,32 @@ async function loadHtml() {
   return html;
 }
 
+async function getRestaurants() {
+  const url = URL.parse(restaurantsApiRoot);
+
+  const opts = {
+    host: url.hostname,
+    path: url.pathname
+  };
+
+  aws4.sign(opts);
+
+  const res = await http
+    .get(restaurantsApiRoot)
+    .set("Host", opts.headers["Host"])
+    .set("X-Amz-Date", opts.headers["X-Amz-Date"])
+    .set("Authorization", opts.headers["Authorization"])
+    .set("X-Amz-Security-Token", opts.headers["X-Amz-Security-Token"]);
+
+  const restaurants = await res.body;
+
+  return restaurants;
+}
+
 module.exports.handler = async event => {
   const template = await loadHtml();
-  const res = await http.get(restaurantsApiRoot);
-  const restaurants = await res.body;
   const dayOfWeek = days[new Date().getDay()];
+  const restaurants = await getRestaurants();
   const html = Mustache.render(template, { dayOfWeek, restaurants });
 
   return {
